8a2de2a1c501e8cb9caf695470083db18cbda2e8
bertinbgc2019 <bertinbgc2019@gmail.com>
2026-09-03 14:11:02 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const CACHE_NAME = 'pos-v1';
-const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
